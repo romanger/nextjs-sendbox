8a3f49283054cf22352fccb709f66d41d8076fce
Roman Gertsvolf <roman.gertsvolf@gmail.com>
2020-09-25 11:40:36 +0300

    posts loading server and front

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,28 @@
+import {useEffect, useState} from "react";
 import {MainLayout} from "../../components/Layouts/MainLayout";
 import {Card} from "../../components/Card";
 
-export default function Blog({posts}) {
+export default function Blog({posts: serverPosts}) {
+    const [posts, setPosts] = useState(serverPosts);
+
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+            const data = await response.json();
+            setPosts(data);
+        }
+
+        if (!serverPosts) {
+            load();
+        }
+    }, []);
+
+    if (!posts) {
+        return (
+            <MainLayout>
+                <p>Loading ..</p>
+            </MainLayout>)
+    }
 
     return (
         <MainLayout title={'Blog | Next try project'}>
@@ -24,7 +45,10 @@ export default function Blog({posts}) {
     )
 };
 
-Blog.getInitialProps = async () => {
+Blog.getInitialProps = async ({req}) => {
+    if (!req) {
+        return {posts: null}
+    }
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const posts = await response.json();
     return {posts}
